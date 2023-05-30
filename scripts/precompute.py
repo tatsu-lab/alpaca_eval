eef55da5c84040cb429387684abb955a35c716d4
Yann Dubois <yanndubois96@gmail.com>
2023-05-30 01:49:25 -0700

    max_instances

--- scripts/precompute.py
+++ scripts/precompute.py
@@ -6,16 +6,17 @@ def precompute_api_crossannotations(
         annotators_configs=(
                 # "gpt4/basic_configs.yaml",
                 # "claude/basic_configs.yaml",
-                # "gpt4/configs.yaml",
+                # "gpt4/b1_configs.yaml",
                 # "gpt3/basic_configs.yaml",
-                # "gpt4/b5_configs.yaml",
+                # "gpt4/configs.yaml",
                 # "chatgpt/basic_configs.yaml",
-                # "vicuna/configs.yaml",
+                "lmsys/configs.yaml",
                 "guanaco-33b/basic_configs.yaml",
-                # "alpaca_farm/configs.yaml",
+                "alpaca_farm/configs.yaml",
                 # "cohere/basic_configs.yaml", # currently doesn't work because only 5 calls per min
-                # "alpaca_farm_greedy-gpt4/configs.yaml",
-        )
+                "alpaca_farm_greedy-gpt4/configs.yaml",
+        ),
+        max_instances=None
 ):
     """Precompute crossannotations for important API models."""
     analyzer = analyze.Analyzer()
@@ -23,7 +24,10 @@ def precompute_api_crossannotations(
     for annotators_config in annotators_configs:
         # saving is done automatically
         _ = analyze.get_crossannotations(
-            analyzer=analyzer, Annotator=annotators.PairwiseAnnotator, annotators_config=annotators_config
+            analyzer=analyzer,
+            Annotator=annotators.PairwiseAnnotator,
+            annotators_config=annotators_config,
+            max_instances=max_instances
         )
 
 
