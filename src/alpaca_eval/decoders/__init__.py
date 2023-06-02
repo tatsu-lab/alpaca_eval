e0c515d30cb60e58b0bd37812f2e75702d12d5cb
Yann Dubois <yanndubois96@gmail.com>
2023-06-01 17:10:11 -0700

    imports

--- src/alpaca_eval/decoders/__init__.py
+++ src/alpaca_eval/decoders/__init__.py
@@ -1,4 +1,5 @@
 from typing import Callable, Union
+from .. import utils
 
 
 def get_fn_completions(name: Union[str, Callable]) -> Callable:
@@ -7,24 +8,37 @@ def get_fn_completions(name: Union[str, Callable]) -> Callable:
         return name
 
     if name == "anthropic_completions":
+        utils.check_imports(["anthropic"], "anthropic_completions")
         from .anthropic import anthropic_completions
 
         return anthropic_completions
+
     elif name == "openai_completions":
+        utils.check_imports(["openai"], "openai_completions")
         from .openai import openai_completions
 
         return openai_completions
+
     elif name == "huggingface_api_completions":
+        utils.check_imports(["huggingface_hub"], "huggingface_api_completions")
         from .huggingface_api import huggingface_api_completions
 
         return huggingface_api_completions
 
     elif name == "huggingface_local_completions":
+        utils.check_imports(
+            ["accelerate", "transformers", "bitsandbytes", "xformers", "optimum", "scipy"],
+            "huggingface_api_completions",
+        )
         from .huggingface_local import huggingface_local_completions
 
         return huggingface_local_completions
 
     elif name == "cohere_completions":
+        utils.check_imports(
+            ["cohere"],
+            "cohere_completions",
+        )
         from .cohere import cohere_completions
 
         return cohere_completions
