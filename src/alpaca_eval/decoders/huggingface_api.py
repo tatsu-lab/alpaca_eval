f1b796253b95860d06d8592d662cf78811e3eedd
Yann Dubois <yanndubois96@gmail.com>
2023-06-02 19:02:06 -0700

    hugging face inference_helper

--- src/alpaca_eval/decoders/huggingface_api.py
+++ src/alpaca_eval/decoders/huggingface_api.py
@@ -1,7 +1,7 @@
 import functools
 import logging
 import multiprocessing
-import os
+import time
 from typing import Sequence
 
 import numpy as np
@@ -57,13 +57,16 @@ def huggingface_api_completions(
     logging.info(f"Kwargs to completion: {default_kwargs}")
 
     with utils.Timer() as t:
+        partial_completion_helper = functools.partial(inference_helper,
+                                                      inference=inference,
+                                                      params=default_kwargs)
         if num_procs == 1:
             completions = [
-                inference(inputs=prompt, params=default_kwargs) for prompt in tqdm.tqdm(prompts, desc="prompts")
+                partial_completion_helper(inputs=prompt) for prompt in
+                tqdm.tqdm(prompts, desc="prompts")
             ]
         else:
             with multiprocessing.Pool(num_procs) as p:
-                partial_completion_helper = functools.partial(inference, params=default_kwargs)
                 completions = list(
                     tqdm.tqdm(
                         p.imap(partial_completion_helper, prompts),
@@ -74,8 +77,7 @@ def huggingface_api_completions(
     logging.info(f"Time for {n_examples} completions: {t}")
 
     completions = [
-        completion[0]["generated_text"] if "error" not in completion else "error:" + completion["error"]
-        for completion in completions
+        completion[0]["generated_text"] for completion in completions
     ]
 
     # unclear pricing
@@ -83,3 +85,22 @@ def huggingface_api_completions(
     avg_time = [t.duration / n_examples] * len(completions)
 
     return dict(completions=completions, price_per_example=price, time_per_example=avg_time)
+
+
+def inference_helper(prompt: str, inference, params, n_retries=100, waiting_time=2) -> dict:
+    for _ in range(n_retries):
+        output = inference(inputs=prompt, params=params)
+        if "error" in output and n_retries > 0:
+            error = output["error"]
+            if "Rate limit reached" in output["error"]:
+                logging.warning(f"Rate limit reached... Trying again in {waiting_time} seconds. Full error: {error}")
+                time.sleep(waiting_time)
+            elif "Input validation error" in error and "max_new_tokens" in error:
+                params["max_new_tokens"] = int(params["max_new_tokens"] * 0.8)
+                logging.warning(f"`max_new_tokens` too large. Reducing target length to {params['max_tokens']}, "
+                                f"Retrying...")
+            else:
+                raise ValueError(f"Error in inference. Full error: {error}")
+        else:
+            return output
+    raise ValueError(f"Error in inference. We tried {n_retries} times and failed.")
