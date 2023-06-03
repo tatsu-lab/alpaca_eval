e10369330ec8f832ee08a4b671252f0ac8aa660b
Yann Dubois <yanndubois96@gmail.com>
2023-06-03 02:43:17 -0700

    precompute

--- scripts/precompute.py
+++ scripts/precompute.py
@@ -2,34 +2,54 @@ from alpaca_eval import utils, metrics, annotators, constants, analyze, main as 
 import fire
 
 
-def precompute_on_all_human_leaderboard(annotators_config="gpt4",
-                                        Annotator=annotators.PairwiseAnnotator,
-                                        all_data=constants.ALPACAFARM_GOLD_ANNOTATIONS,
-                                        analyzer_kwargs=None,
-                                        **annotator_kwargs):
+def precompute_on_all_human_leaderboard(
+        annotators_config="gpt4",
+        Annotator=annotators.PairwiseAnnotator,
+        all_data=constants.ALPACAFARM_GOLD_ANNOTATIONS,
+        analyzer_kwargs=None,
+        **annotator_kwargs
+):
     """Precompute all instructions on the eval leaderboard that has been annotated by humans."""
     analyzer_kwargs = analyzer_kwargs or {}
     analyzer = analyze.Analyzer(gold_annotations=all_data, **analyzer_kwargs)
-    df_annotations = analyze.get_annotations(analyzer,
-                                             Annotator=Annotator,
-                                             annotators_config=annotators_config,
-                                             **annotator_kwargs)
+    df_annotations = analyze.get_annotations(
+        analyzer, Annotator=Annotator, annotators_config=annotators_config, **annotator_kwargs
+    )
 
 
-def precompute_evaluator_leaderboard(annotators_configs="EVALUATORS_TO_BENCHMARK",
-                                     max_instances=None,
-                                     **kwargs):
+def precompute_evaluator_leaderboard(
+        annotators_configs_to_analyze="EVALUATORS_LEADERBOARD",
+        annotators_configs_to_benchmark="EVALUATORS_TO_BENCHMARK",
+        max_instances=None,
+        **kwargs
+):
     """Precompute evaluator's leaderboard for important API models."""
-    if isinstance(annotators_configs, str):
-        annotators_configs = getattr(constants, annotators_configs)
+    if isinstance(annotators_configs_to_analyze, str):
+        annotators_configs_to_analyze = getattr(constants, annotators_configs_to_analyze)
 
-    for annotators_config in annotators_configs:
+    if isinstance(annotators_configs_to_benchmark, str):
+        annotators_configs_to_benchmark = getattr(constants, annotators_configs_to_benchmark)
+
+    for annotators_config in annotators_configs_to_analyze:
         # saving is done automatically
-        _ = alpaca_main.analyze_evaluators(annotators_config=annotators_config,
-                                           max_instances=max_instances,
-                                           is_save_leaderboard=max_instances is None,
-                                           is_return_instead_of_print=True,  # don't print
-                                           **kwargs)
+        _ = alpaca_main.analyze_evaluators(
+            annotators_config=annotators_config,
+            max_instances=max_instances,
+            is_save_leaderboard=max_instances is None,
+            is_return_instead_of_print=True,  # don't print
+            **kwargs
+        )
+
+    for annotators_config in annotators_configs_to_benchmark:
+        # saving is done automatically
+        _ = alpaca_main.analyze_evaluators(
+            annotators_config=annotators_config,
+            max_instances=max_instances,
+            is_save_leaderboard=max_instances is None,
+            is_return_instead_of_print=True,  # don't print
+            is_single_annotator=True,
+            **kwargs
+        )
 
 
 def main(task, **kwargs):
