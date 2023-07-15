6ecae95a11645e683f5ef88c0f74d0ba351bee09
Yann Dubois <yanndubois96@gmail.com>
2023-07-15 14:15:28 +0200

    git add format_sample_sheets.py

---
+++ docs/format_sample_sheets.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pandas as pd
+
+BASE_DIR = Path(__file__).parents[1]
+RESULTS_DIR = BASE_DIR / "results"
+F_OUTPUTS = "model_outputs.json"
+
+
+df_reference = pd.read_json(RESULTS_DIR / "text_davinci_003" / F_OUTPUTS, orient="records")
+
+
+# Create a dict mapping each instruction in df_reference to its index => will keep that order for the other files
+order = {tuple(pair): i for i, pair in enumerate(zip(df_reference["dataset"], df_reference["instruction"]))}
+
+for f in RESULTS_DIR.glob(f"*/{F_OUTPUTS}"):
+    df = pd.read_json(f, orient="records")
+    if len(df_reference) != len(df):
+        raise ValueError(f"Length of {f} is not equal to the reference file {len(df_reference)}!={len(df)}.")
+
+    # Sort the df using the reference df
+    df["order"] = df.apply(lambda row: order[(row["dataset"], row["instruction"])], axis=1)
+    df = df.sort_values("order").drop("order", axis=1)
+
+    df.to_json(f, orient="records", indent=2)
