a66102f131235b5eef38872e50b337fc6dcaf60c
Yann Dubois <yanndubois96@gmail.com>
2023-07-20 04:33:03 +0200

    [ENH] add llama 70B outputs (#91)
    * [ENH] add replicate decoder
    
    * add 70B llama results
    
    * add  outputs from alpaca chat 70B

--- src/alpaca_eval/decoders/replicate.py
+++ src/alpaca_eval/decoders/replicate.py
@@ -15,7 +15,7 @@ __all__ = ["replicate_completions"]
 def replicate_completions(
     prompts: Sequence[str],
     model_name: str,
-    num_procs: int = 100,
+    num_procs: int = 16,
     **decoding_kwargs,
 ) -> dict[str, list]:
     r"""Get completions using a model hosted on https://replicate.com/.
