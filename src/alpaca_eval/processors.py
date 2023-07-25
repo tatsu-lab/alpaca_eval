af2d72c986c13854a5ba92c65ecc6482e14207a2
Yann Dubois <yanndubois96@gmail.com>
2023-07-25 11:30:14 +0200

    ANNOTATOR_COLUMN all caps

--- src/alpaca_eval/processors.py
+++ src/alpaca_eval/processors.py
@@ -14,6 +14,7 @@ import pandas as pd
 from . import utils
 
 DUMMY_EXAMPLE = dict(instruction="1+1=", output_1="2", input="", output_2="3")
+__all__ = ["RandomSwitchTwoColumnsProcessor", "PaddingForBatchesProcessor"]
 
 
 class BaseProcessor(abc.ABC):
