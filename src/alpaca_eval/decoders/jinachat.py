04ab474e6eafd1ee92ba0b0f4e49c2433caacf27
jupyterjazz <saba.sturua@jina.ai>
2023-08-09 16:07:29 +0200

    refactor: apply suggestions
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- src/alpaca_eval/decoders/jinachat.py
+++ src/alpaca_eval/decoders/jinachat.py
@@ -1,7 +1,7 @@
 import logging
 import multiprocessing
 from functools import partial
-from typing import Sequence
+from typing import Sequence, Optional
 import requests
 import json
 import os
@@ -14,6 +14,7 @@ __all__ = ["jina_chat_completions"]
 
 def jina_chat_completions(
     prompts: Sequence[str],
+    num_procs: Optional[int] = 4,
 ) -> dict[str, list]:
     """Get jina chat completions for the given prompts. Allows additional parameters such as tokens to avoid and
     tokens to favor.
@@ -22,6 +23,8 @@ def jina_chat_completions(
     ----------
     prompts : list of str
         Prompts to get completions for.
+    num_procs : int, optional
+        Number of parallel processes to use for decoding.
     """
     n_examples = len(prompts)
     api_key = os.environ.get('JINA_CHAT_API_KEY')
@@ -33,20 +36,23 @@ def jina_chat_completions(
         logging.info(f"Using `jina_chat_completions` on {n_examples} prompts.")
 
     prompts = [_prompt_to_chatml(prompt.strip()) for prompt in prompts]
-    num_processes = min(multiprocessing.cpu_count(), 4)
+    num_processes = min(multiprocessing.cpu_count(), num_procs)
     with utils.Timer() as t:
         with multiprocessing.Pool(processes=num_processes) as pool:
-            print(f"Number of processes: {pool._processes}")
+            logging.info(f"Number of processes: {pool._processes}")
             get_chat_completion_with_key = partial(_get_chat_completion, api_key)
-            completions = pool.map(get_chat_completion_with_key, prompts)
+            completions_and_num_tokens = pool.map(get_chat_completion_with_key, prompts)
+
+    completions = [text for text, _ in completions_and_num_tokens]
+    num_tokens = [tokens for _, tokens in completions_and_num_tokens]
 
     logging.info(f"Completed {n_examples} examples in {t}.")
 
     # refer to https://chat.jina.ai/billing
-    price = 0
+    price_per_example = [0.08 if msg_tokens > 300 else 0 for msg_tokens in num_tokens]
     avg_time = [t.duration / n_examples] * len(completions)
 
-    return dict(completions=completions, price_per_example=price, time_per_example=avg_time)
+    return dict(completions=completions, price_per_example=price_per_example, time_per_example=avg_time)
 
 
 def _get_chat_completion(api_key, prompt):
@@ -63,14 +69,16 @@ def _get_chat_completion(api_key, prompt):
         try:
             response = requests.post(url, headers=headers, json=json_payload)
             response.raise_for_status()  # Will raise an HTTPError if one occurred.
-            return response.json()['choices'][0]['message']['content']
+            message = response.json()['choices'][0]['message']['content']
+            message_tokens = response.json()['usage']['completion_tokens']
+            return message, message_tokens
         except (json.JSONDecodeError, requests.exceptions.HTTPError) as e:
-            print(f"Error occurred: {e}, Attempt {attempt + 1} of {max_retries}")
+            logging.warning(f"Error occurred: {e}, Attempt {attempt + 1} of {max_retries}")
             time.sleep(5)
             if attempt + 1 == max_retries:
-                print("Max retries reached. Raising exception.")
-                print(f"Request data -> URL: {url}, Headers: {headers}, JSON Payload: {json_payload}")
+                logging.exception("Max retries reached. Raising exception.")
+                logging.exception(f"Request data -> URL: {url}, Headers: {headers}, JSON Payload: {json_payload}")
                 raise
         except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+            logging.exception(f"An unexpected error occurred: {e}")
             raise
