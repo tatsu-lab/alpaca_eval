84293a2e29af29d7ee93035a7f4ff100973dabc7
Sander Land <sander@cohere.com>
2023-09-22 09:41:29 +0000

    updated results and leaderboard

--- src/alpaca_eval/decoders/cohere.py
+++ src/alpaca_eval/decoders/cohere.py
@@ -5,7 +5,7 @@ import math
 import multiprocessing
 import os
 import random
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Tuple
 
 import cohere
 import tqdm
@@ -17,12 +17,11 @@ __all__ = ["cohere_completions"]
 
 
 def cohere_completions(
-        prompts: Sequence[str],
-        model_name="command",
-        mode = "instruct",
-        num_procs: int = 5,
-        staging=False,
-        **decoding_kwargs,
+    prompts: Sequence[str],
+    model_name="command",
+    mode = "instruct",
+    num_procs: int = 5,
+    **decoding_kwargs,
 ) -> dict[str, list]:
     """Decode with Cohere API.
 
@@ -47,17 +46,16 @@ def cohere_completions(
     else:
         logging.info(f"Using `cohere_completions` on {n_examples} prompts using {model_name}.")
 
-    kwargs = dict(model=model_name, mode=mode, staging=staging, **decoding_kwargs)
+    kwargs = dict(model=model_name, mode=mode, **decoding_kwargs)
     logging.info(f"Kwargs to completion: {kwargs}")
 
-    num_procs= 1
     with utils.Timer() as t:
         if num_procs == 1:
-            completions = [_cohere_completion_helper(prompt, **kwargs) for prompt in tqdm.tqdm(prompts, desc="prompts")]
+            completions_and_token_counts = [_cohere_completion_helper(prompt, **kwargs) for prompt in tqdm.tqdm(prompts, desc="prompts")]
         else:
             with multiprocessing.Pool(num_procs) as p:
                 partial_completion_helper = functools.partial(_cohere_completion_helper, **kwargs)
-                completions = list(
+                completions_and_token_counts = list(
                     tqdm.tqdm(
                         p.imap(partial_completion_helper, prompts),
                         desc="prompts",
@@ -65,29 +63,26 @@ def cohere_completions(
                     )
                 )
     logging.info(f"Completed {n_examples} examples in {t}.")
-
+    completions, num_tokens = zip(*completions_and_token_counts)
     # cohere charges $2.5 for every 1000 call to API that is less than 1000 characters. Only counting prompts here
-    price = [2.5 / 1000 * math.ceil(len(prompt) / 1000) for prompt in prompts]
+    price_per_token = 0.000015
+    non_err_num_tokens = [n for n in num_tokens if n] or [0] # give 0 if not available, eg chat
+    price = price_per_token * sum(non_err_num_tokens) / len(non_err_num_tokens)
     avg_time = [t.duration / n_examples] * len(completions)
 
     return dict(completions=completions, price_per_example=price, time_per_example=avg_time)
 
 
 def _cohere_completion_helper(
-        prompt: str,
-        cohere_api_keys: Optional[Sequence[str]] = (constants.COHERE_API_KEY,),
-        max_tokens: Optional[int] = 1000,
-        temperature: Optional[float] = 0.7,
-        max_tries = 5,
-        mode = "instruct",
-        staging=False,
-        **kwargs,
-) -> str:
+    prompt: str,
+    cohere_api_keys: Optional[Sequence[str]] = (constants.COHERE_API_KEY,),
+    max_tokens: Optional[int] = 1000,
+    temperature: Optional[float] = 0.7,
+    max_tries = 5,
+    mode = "instruct",
+    **kwargs,
+) -> Tuple[str,int]:
     cohere_api_key = random.choice(cohere_api_keys)
-    if staging:
-        cohere.COHERE_API_URL = "https://stg.api.cohere.ai"
-    else:
-        cohere.COHERE_API_URL = "https://api.cohere.ai"
     client = cohere.Client(cohere_api_key)
 
     kwargs.update(dict(max_tokens=max_tokens, temperature=temperature))
@@ -96,20 +91,22 @@ def _cohere_completion_helper(
     for trynum in range(max_tries):  # retry errors
         try:
             if mode == "instruct":
-                response = client.generate(prompt=prompt, **curr_kwargs)
+                response = client.generate(prompt=prompt, return_likelihoods="ALL", **curr_kwargs)
                 text = response[0].text
+                num_tokens = len(response[0].token_likelihoods)
             elif mode == "chat":
                 response = client.chat(prompt, **curr_kwargs)
                 text = response.text
+                num_tokens = 0 # not implemented for chat
             else:
                 raise ValueError(f"Invalid mode {mode} for cohere_completions")
 
             if text == "":
                 raise CohereError("Empty string response")
 
-            return text
+            return text, num_tokens
 
         except CohereError as e:
             print(f"Try #{trynum+1}/{max_tries}: Error running prompt {repr(prompt)}: {e}")
 
-    return " "  # placeholder response for errors, doesn't allow empty string
+    return " ", 0  # placeholder response for errors, doesn't allow empty string
