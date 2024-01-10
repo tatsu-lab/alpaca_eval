e3771ad64d7f46f4565eabd7f4e293ebab360ffe
Yann Dubois <yanndubois96@gmail.com>
2024-01-10 13:09:05 -0800

    [WIP] precompute all leaderboard for AE2 (#199)
    * precompute all leaderboard for AE2
    
    * precompute all leaderboard for AE2
    
    * precompute all leaderboard for AE2
    
    * tulu
    
    * adding models
    
    * adding OpenHermes-2.5-Mistral-7B
    
    * adding models
    
    * making both leaderboards the same
    
    * adding all the annotations
    
    * adding fixed gpt4 versions
    
    * fix all annotations
    
    * update models
    
    * push all the leaderboards

--- scripts/precompute.py
+++ scripts/precompute.py
@@ -1,8 +1,13 @@
+from typing import Optional
+
 import fire
+import pandas as pd
+from scipy.stats import pearsonr, spearmanr
 
 from alpaca_eval import analyze, annotators, constants
 from alpaca_eval import main as alpaca_main
 from alpaca_eval import metrics, utils
+from alpaca_eval.types import AnyPath
 
 
 def precompute_on_all_human_leaderboard(
@@ -10,7 +15,7 @@ def precompute_on_all_human_leaderboard(
     Annotator=annotators.PairwiseAnnotator,
     all_data=constants.ALPACAFARM_GOLD_ANNOTATIONS,
     analyzer_kwargs=None,
-    **annotator_kwargs
+    **annotator_kwargs,
 ):
     """Precompute all instructions on the eval leaderboard that has been annotated by humans."""
     analyzer_kwargs = analyzer_kwargs or {}
@@ -24,7 +29,7 @@ def precompute_evaluator_leaderboard(
     annotators_configs_to_analyze="MINIMAL_EVALUATORS",
     annotators_configs_to_benchmark="VERIFIED_EVALUATORS",
     max_instances=None,
-    **kwargs
+    **kwargs,
 ):
     """Precompute evaluator's leaderboard for important API models."""
     if isinstance(annotators_configs_to_analyze, str):
@@ -41,7 +46,7 @@ def precompute_evaluator_leaderboard(
             is_save_leaderboard=max_instances is None,
             is_return_instead_of_print=True,  # don't print
             current_leaderboard_mode="minimal",
-            **kwargs
+            **kwargs,
         )
 
     for annotators_config in annotators_configs_to_benchmark:
@@ -53,7 +58,7 @@ def precompute_evaluator_leaderboard(
             is_return_instead_of_print=True,  # don't print
             is_single_annotator=True,
             current_leaderboard_mode="verified",
-            **kwargs
+            **kwargs,
         )
 
 
@@ -62,6 +67,36 @@ def update_leaderboard(leaderboard_path, model_outputs="results/{model_name}/mod
     df_leaderboard = utils.load_or_convert_to_dataframe(leaderboard_path)
     for model_name in df_leaderboard.index:
         alpaca_main.evaluate(model_outputs=model_outputs.format(model_name=model_name), **kwargs)
+
+
+def compare_leaderboards(leaderboard_path_1, leaderboard_path_2):
+    df_lb_1 = utils.load_or_convert_to_dataframe(leaderboard_path_1)
+    df_lb_2 = utils.load_or_convert_to_dataframe(leaderboard_path_2)
+
+    # keep only intersection of models and in the same order
+    intersected_models = df_lb_1.index.intersection(df_lb_2.index)
+    df_lb_1 = df_lb_1.loc[intersected_models]
+    df_lb_2 = df_lb_2.loc[intersected_models]
+
+    metrics = {}
+    metrics["Spearman corr."] = spearmanr(df_lb_1["win_rate"], df_lb_2["win_rate"]).statistic
+    metrics["Pearson corr."] = pearsonr(df_lb_1["avg_length"], df_lb_2["avg_length"]).statistic
+
+    print(pd.Series(metrics).to_string(float_format="%.2f"))
+
+
+def make_leaderboard_like(leaderboard_to_copy: Optional[AnyPath], **kwargs):
+    """Make a leaderboard on all the models that have been evaluated in another leaderboard."""
+    df_lb_old = pd.read_csv(leaderboard_to_copy, index_col=0)
+
+    kwargs["is_cache_leaderboard"] = True
+    kwargs["is_return_instead_of_print"] = True
+    for m, r in df_lb_old.iterrows():
+        kwargs["current_leaderboard_mode"] = r["mode"]
+        leaderboard_new, _ = alpaca_main.evaluate(model_outputs=f"results/{m}/model_outputs.json", **kwargs)
+
+    print("Comparison between the leaderboards:")
+    compare_leaderboards(leaderboard_to_copy, leaderboard_new)
 
 
 def main(task, **kwargs):
