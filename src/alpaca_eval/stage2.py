98ae463dca4ff9079701ff768875fc825e927221
Sambuddha Roy <sambroy@microsoft.com>
2024-02-07 13:40:03 -0800

    merging with sambroy/ae2

--- src/alpaca_eval/stage2.py
+++ src/alpaca_eval/stage2.py
@@ -38,6 +38,16 @@ CUR_DIR = Path(__file__).parent
 __all__ = ["evaluate", "evaluate_from_model", "analyze_evaluators", "make_leaderboard"]
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ["yes", "true", "t", "y", "1"]:
+        return True
+    elif v.lower() in ["no", "false", "f", "n", "0"]:
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 def parse_args():
     # Create the argument parser
     parser = argparse.ArgumentParser()
@@ -45,6 +55,7 @@ def parse_args():
     # Define arguments based on the YAML spec
     # For inputs; here the inputs are model and reference model outputs.
     parser.add_argument("--model_outputs", type=str, help="Path to the directory containing the model outputs", required=False)
+    parser.add_argument("--use_alpaca_eval_1", type=str2bool, help="Whether to use the alpaca_eval_1 or eval_2; by default set to False.", required=False, default=False)
 
     # For outputs
     parser.add_argument("--output_dir", type=str, help="Output directory")
@@ -70,6 +81,16 @@ def stage2_main():
     print(f"Path to model + reference model outputs: {model_outputs_path}")
     print(f"Output Directory: {output_dir}")
 
+    if args.use_alpaca_eval_1:
+        annotator_config_file = "alpaca_eval_gpt4"
+    else:
+        annotator_config_file = "weighted_alpaca_eval_gpt4_turbo"
+    
+    # # hardcoding it to check on AML first.
+    # annotator_config_file = "weighted_alpaca_eval_gpt4_turbo"
+
+    logging.info(f"Using annotator_config_file = {annotator_config_file}")
+
 
     # convert from json to pandas dataframe; this is what the evaluate function expects
     model_outputs_file = os.path.join(args.model_outputs, "model_outputs.json")
@@ -88,7 +109,7 @@ def stage2_main():
     evaluate(
         model_outputs=model_outputs_df,
         reference_outputs=reference_model_outputs_df,
-        annotators_config="alpaca_eval_gpt4",
+        annotators_config=annotator_config_file,
         output_path=output_dir,
         max_instances=None,
     )
